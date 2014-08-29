5dad71fb135e50258f18910702ee33b19cddb105
epitsin <epitsin@yahoo.com>
2014-08-29 13:47:02 +0300

    Added folder repositories with generic repository and interfaces

--- IceElemental/FurnitureSystem.Data/FurnitureSystemDbContext.cs
+++ IceElemental/FurnitureSystem.Data/FurnitureSystemDbContext.cs
@@ -4,12 +4,69 @@ namespace FurnitureSystem.Data
     using System.Data.Entity;
     using System.Linq;
 
-    public class FurnitureSystemDbContext : DbContext
+    public class FurnitureSystemDbContext : DbContext, IFurnitureSystemDbContext
     {
         public FurnitureSystemDbContext()
             : base("FurnitureSystem")
         {
         }
 
+        public IDbSet<Models.Manufacturer> Manufacturers
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IDbSet<Models.FurniturePiece> FurniturePieces
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IDbSet<Models.Shop> Shops
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IDbSet<Models.Price> Prices
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public new IDbSet<T> Set<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public new void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ IceElemental/FurnitureSystem.Data/IFurnitureSystemDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace FurnitureSystem.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    using FurnitureSystem.Models;
+
+    public interface IFurnitureSystemDbContext
+    {
+        IDbSet<Manufacturer> Manufacturers { get; set; }
+
+        IDbSet<FurniturePiece> FurniturePieces { get; set; }
+
+        IDbSet<Shop> Shops { get; set; }
+
+        IDbSet<Price> Prices { get; set; }
+
+        IDbSet<T> Set<T>() where T : class;
+
+        DbEntityEntry<T> Entry<T>(T entity) where T : class;
+
+        void SaveChanges();
+    }
+}
