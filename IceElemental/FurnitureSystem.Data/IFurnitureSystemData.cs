96c0298b09c5d5901985e0c4cf1df44a65c896fa
epitsin <epitsin@yahoo.com>
2014-08-29 13:55:50 +0300

    added class funrnitureSystemData and interface for it

---
+++ IceElemental/FurnitureSystem.Data/IFurnitureSystemData.cs
@@ -0,0 +1,10 @@
+﻿namespace FurnitureSystem.Data
+{
+    using FurnitureSystem.Data.Repositories;
+    using FurnitureSystem.Models;
+
+    public interface IFurnitureSystemData
+    {
+        IGenericRepository<FurniturePiece> FurniturePieces { get; } // ??????????
+    }
+}
