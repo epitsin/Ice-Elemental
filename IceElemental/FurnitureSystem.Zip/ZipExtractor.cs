ef9f1a4c97bee65d405f91653dca3c0174530cd7
epitsin <epitsin@yahoo.com>
2014-09-01 13:12:18 +0300

    added the zip and excel files to the folders

---
+++ IceElemental/FurnitureSystem.Zip/ZipExtractor.cs
@@ -0,0 +1,23 @@
+﻿namespace FurnitureSystem.Zip
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipExtractor
+    {
+        public static void Extract(string zipPath, string extractPath)
+        {
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName.EndsWith(".xls", StringComparison.OrdinalIgnoreCase))
+                    {
+                        entry.ExtractToFile(Path.Combine(extractPath, entry.FullName));
+                    }
+                }
+            } 
+        }
+    }
+}
