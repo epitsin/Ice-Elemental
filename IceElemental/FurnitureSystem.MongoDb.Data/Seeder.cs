063575a1fe1b1c226365ddd57bd49aa5fd892248
epitsin <epitsin@yahoo.com>
2014-09-03 12:46:33 +0300

    mongo base

--- IceElemental/FurnitureSystem.MongoDb.Data/Seeder.cs
+++ IceElemental/FurnitureSystem.MongoDb.Data/Seeder.cs
@@ -18,11 +18,16 @@
         {
             var innitialShops = new List<ShopLocation> 
             {
-                new ShopLocation("Alfredo", "Bulgaria", "Sofia", "Malinov", 50),
-                new ShopLocation("Kaspichan", "China", "Mimi", "Malinov", 34),
-                new ShopLocation("Kifla", "Irak", "Pepi", "Malinov", 23),
-                new ShopLocation("Something", "Nigeria", "Jiji", "Malinov", 12),
-                new ShopLocation("Pesho", "Kaspichan", "Sofia", "Malinov", 5)
+                new ShopLocation("Trendy Shop", "Bulgaria", "Sofia", "ul. Tsvetna gradina 2", 50, 0.1m),
+                new ShopLocation("Mediocre Shop", "Bulgaria", "Plovdiv", "kv. Kiochuk Parizh", 34, 0.15m),
+                new ShopLocation("Shop Za Naroda", "Bulgaria", "Pernik", "mestnost Do Zavoda", 23, 0.3m),
+                new ShopLocation("Very Expensive Furniture", "Bulgaria", "Sofia", "ul. Fritiof Nansen 6", 12, 0.23m),
+                new ShopLocation("Effecta", "Bulgaria", "Sofia", "bul. Evlogi Georgiev 34", 5, 0.2m),
+                new ShopLocation("Kitaiski Mebeli", "Bulgaria", "Burgas", "ul. Kitaiska Stena 4", 45, 0.4m),
+                new ShopLocation("Furrniture Shop", "Bulgaria", "Stara Zagora", "bul. Simeon Veliki 6", 30, 0.27m),
+                new ShopLocation("Dolce Casa", "Bulgaria", "Sofia", "bul. Bulgaria 23", 47, 0.23m),
+                new ShopLocation("Casa Viva", "Bulgaria", "Varna", "ul.3ti mart 20", 41, 0.17m),
+                new ShopLocation("Platan", "Bulgaria", "Sofia", "ul. Saborna 14", 36, 0.3m)
             };
 
             if (this.dbContext.GetCollection("ShopSystem") == null)
